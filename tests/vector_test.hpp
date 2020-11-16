4ab6f4841294e8b4fed9e02fe05a109398686413
Andrei Dimaano <andreiidimaano@gmail.com>
2020-11-16 15:06:45 -0800

    add at tests

--- tests/vector_test.hpp
+++ tests/vector_test.hpp
@@ -1,5 +1,12 @@
 #include "gtest/gtest.h"
  
+#include "../composite/header/add.hpp"
+#include "../composite/header/div.hpp"
+#include "../composite/header/mult.hpp"
+#include "../composite/header/op.hpp"
+#include "../composite/header/pow.hpp"
+#include "../composite/header/rand.hpp"
+#include "../composite/header/sub.hpp"
 #include "../VectorContainer.hpp"
 
 //Default constructor test
@@ -10,10 +17,41 @@ TEST(ClassVectorContainerTest, vectorDefaultConstructor) {
 
 //Swap
 
+
 //Print
 
 //Size
 
 //at
+TEST(VectorContainerTestSet, vectorAtOpTest) {
+    // Setup the elements under test
+    Op* seven = new Op(7);
+    VectorContainer* test_container = new VectorContainer();
+
+    // Exercise some functionality of hte test elements
+    test_container->add_element(seven);
+
+    // Assert that the container has at least a single element
+    // otherwise we are likely to cause a segfault when accessing
+    ASSERT_EQ(test_container->size(), 1);
+    EXPECT_EQ(test_container->at(0)->evaluate(), 7);
+}
+TEST(VectorContainerTestSet, vectorAtAddTest) {
+    // Setup the elements under test
+    Base* seven = new Op(7);
+    Base* three = new Op(3);
+    Base* sum = new Add(seven, three);
+
+    VectorContainer* test_container = new VectorContainer();
+
+    // Exercise some functionality of hte test elements
+    test_container->add_element(sum);
+
+    // Assert that the container has at least a single element
+    // otherwise we are likely to cause a segfault when accessing
+    ASSERT_EQ(test_container->size(), 1);
+    EXPECT_EQ(test_container->at(0)->evaluate(), 10);
+}
+
 
 //add element
