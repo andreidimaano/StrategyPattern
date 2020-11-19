4200a216a798893ba9793e4470f783a715b24193
andreidimaano <andreiidimaano@gmail.com>
2020-11-18 20:54:03 -0800

    finish tests

--- tests/bubble_tests.hpp
+++ tests/bubble_tests.hpp
@@ -8,6 +8,7 @@
 #include "../composite/header/rand.hpp"
 #include "../composite/header/sub.hpp"
 #include "../VectorContainer.hpp"
+#include "../ListContainer.hpp"
 #include "../BubbleSort.hpp"
 #include "../sort.hpp"
 #include <iostream>
@@ -78,4 +79,36 @@ TEST(SortTestSet, NullBubbleSortTest) {
 
 	ASSERT_EQ(container->size(), 3);
 	EXPECT_THROW(container->sort(), const char*);
+}
+
+TEST(SortTestSet, ListBubbleSortTest) {
+	Op* seven = new Op(7);
+	Op* four = new Op(4);
+	Mult* TreeA = new Mult(seven, four);
+
+	Op* three = new Op(3);
+	Op* two = new Op(2);
+	Add* TreeB = new Add(three, two);
+
+	Op* ten = new Op(10);
+	Op* six = new Op(6);
+	Sub* TreeC = new Sub(ten, six);
+
+	ListContainer* container = new ListContainer();
+	container->add_element(TreeA);
+	container->add_element(TreeB);
+	container->add_element(TreeC);
+
+	ASSERT_EQ(container->size(), 3);
+	EXPECT_EQ(container->at(0)->evaluate(), 28);
+	EXPECT_EQ(container->at(1)->evaluate(), 5);
+	EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+	container->set_sort_function(new BubbleSort());
+	container->sort();
+
+	ASSERT_EQ(container->size(), 3);	
+	EXPECT_EQ(container->at(0)->evaluate(), 4);
+	EXPECT_EQ(container->at(1)->evaluate(), 5);
+	EXPECT_EQ(container->at(2)->evaluate(), 28);
 }
--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -3,6 +3,7 @@
 #include "vector_tests.hpp"
 #include "sort_tests.hpp"
 #include "list_test.hpp"
+#include "bubble_tests.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
