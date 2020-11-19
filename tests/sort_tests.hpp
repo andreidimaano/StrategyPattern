5b2d2502cd481a11a8aeca96f943f01d4281b84a
Andrei Dimaano <andreiidimaano@gmail.com>
2020-11-19 12:02:16 -0800

    add selection sort list tests

--- tests/sort_tests.hpp
+++ tests/sort_tests.hpp
@@ -82,3 +82,72 @@ TEST(SortTestSet, NullSelectionSortTest) {
     ASSERT_EQ(container->size(), 3);
     EXPECT_THROW(container->sort(),const char*);
 }
+
+TEST(SortTestSet, ListSelectionSortTest) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    ListContainer* container = new ListContainer();
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+    
+    container->set_sort_function(new SelectionSort());
+    container->sort();
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 4);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 28);
+}
+
+TEST(SortTestSet, ListNullSelectionSortTest) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    ListContainer* container = new ListContainer();
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+    //container->set_sort_function(new SelectionSort());
+    //container->sort();
+
+    try {
+        container->sort();
+    } catch(const char* msg) {
+        std::cout << msg << std::endl;
+    }
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_THROW(container->sort(),const char*);
+}
